b9b18a25f8417ae4785edbe15c85bfad2144a595
ThomasKnoops <thomas@knoops.be>
2024-08-15 19:47:05 +0200

    feature: added 3th screen

--- decipher.py
+++ decipher.py
@@ -5,20 +5,11 @@ def xor_with_word(xor_result_bytes, word):
     word_bytes = word.encode('utf-8')
     return hf.xor_bytes(xor_result_bytes, word_bytes)
 
-# Print the results in a readable format, and give the target cipher a special mark
-def print_results(results):
-    total_items = len(results)
-    
-    for index, (key, value) in enumerate(results.items()):
-        if index == total_items - 1:
-            print(f"{key} (!Target Cipher!): {value}")
-        else:
-            print(f"{key}: {value}")
 
 # Decipher the target cipher using the sentences and all the ciphers, interactively
 def decipher(sentence, start_cipher, cipher_array):    
     start_cipher_bytes = hf.hex_to_bytes(start_cipher)
-    results = {}
+    results_string = []
 
     # XOR each ciphertext with start_cipher and then XOR the result with the sentence
     for i, cipher in enumerate(cipher_array):
@@ -27,7 +18,6 @@ def decipher(sentence, start_cipher, cipher_array):
         result_after_XOR_2 = xor_with_word(result_after_XOR_1, sentence)
         
         result_str = result_after_XOR_2.decode('utf-8', errors='ignore')
-        results[f'Cipher {i + 1}'] = result_str
+        results_string.append(result_str)
 
-    print_results(results)
-    return results
+    return results_string
--- main.py
+++ main.py
@@ -19,6 +19,7 @@ target_value = tk.IntVar(value=0)
 combobox_value = None
 text_box_word = None
 cipher_word_progress = []
+combobox_cipher = None
 
 def setup_gui_screen_1():
     global current_frame, text_area, target_value, combobox_value
@@ -133,12 +134,11 @@ def setup_gui_screen_2():
 def try_first_word():
     global text_box_word, cipher_word_progress
     return_cipher_xor_word = mpa.multiPadAttack(text_box_word.get(), CIPHERS)
-    print(return_cipher_xor_word)
     for i, result in enumerate(return_cipher_xor_word):
         cipher_word_progress[i].set("XOR with c" + str(i + 1) + ":\n" + result)
 
 def setup_gui_screen_3():
-    global current_frame
+    global current_frame, text_box_word, combobox_cipher
     if current_frame:
         current_frame.destroy()
 
@@ -147,8 +147,45 @@ def setup_gui_screen_3():
     current_frame.grid(sticky="nsew")
 
     # Add info label
-    ttk.Label(current_frame, text="Solve the ciphers by completing them.").grid(column=0, row=0, pady=(0, 10))
+    ttk.Label(current_frame, text="Now keep guessing the next part of the ciphers.").grid(column=0, row=0, pady=(0, 10))
     
+    # Create text box and combobox
+    guess_frame = ttk.Frame(current_frame)
+    guess_frame.grid(column=0, row=1, pady=(0, 10))
+    text_box_word = ttk.Entry(guess_frame, width=100)
+    text_box_word.grid(column=0, row=0, pady=(0, 10))
+    combobox_cipher = ttk.Combobox(guess_frame, values=[f"c{i + 1}" for i in range(len(CIPHERS))], state="readonly")
+    combobox_cipher.set("c1")
+    combobox_cipher.grid(column=1, row=0, pady=(0, 10))
+
+    # Cipher Progress
+    cipher_frame = ttk.Frame(current_frame)
+    cipher_frame.grid(column=0, row=2, pady=(0, 10))
+    for i, cipher in enumerate(CIPHERS):
+        cipher_word_progress.append(tk.StringVar(value="XOR with c" + str(i + 1) + ": "))
+        ttk.Label(cipher_frame, textvariable=cipher_word_progress[i]).grid(column=i * 2, row=0, pady=(0, 5))
+        if i < len(CIPHERS) - 1:
+            separator = ttk.Separator(cipher_frame, orient='vertical')
+            separator.grid(column=(i * 2) + 1, row=0, sticky='ns', padx=(5, 0))
+
+    # Add buttons
+    button_frm = ttk.Frame(current_frame)
+    button_frm.grid(column=0, row=3, pady=(10, 0))
+
+    ttk.Button(button_frm, text="Guess", command=guess).grid(column=0, row=0, pady=(10, 0))
+    ttk.Button(button_frm, text="Save to file", command=save_to_file).grid(column=1, row=0, pady=(10, 0))
+    ttk.Button(button_frm, text="Quit", command=ROOT.destroy).grid(column=2, row=0, pady=(10, 0))
+
+    # Configure grid stretching
+    current_frame.rowconfigure(0, weight=1)
+    current_frame.columnconfigure(0, weight=1)
+
+def guess():
+    global text_box_word, combobox_cipher, cipher_word_progress
+    return_cipher_xor_word = dec.decipher(text_box_word.get(), CIPHERS[int(combobox_cipher.get()[1:]) - 1], CIPHERS)
+    print(return_cipher_xor_word)
+    for i, result in enumerate(return_cipher_xor_word):
+        cipher_word_progress[i].set("XOR with c" + str(i + 1) + ":\n" + result)    
 
 def save_to_file(data, filename):
     with open(filename, 'w') as file:
